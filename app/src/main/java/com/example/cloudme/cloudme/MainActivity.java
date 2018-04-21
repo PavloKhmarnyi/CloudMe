724e8024ce5f352276af64ec02a4c317b5eb2303
PavloKhmarnyi <pavlo.khmarny@gmail.com>
2018-04-22 00:57:40 +0300

    Prepare basic packages for project #9

--- app/src/main/java/com/example/cloudme/cloudme/MainActivity.java
+++ app/src/main/java/com/example/cloudme/cloudme/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cloudme.cloudme;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.cloudme.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
