859ec13200567f03433f7458b27a9126c9533a53
PavloKhmarnyi <pavlo.khmarny@gmail.com>
2018-04-29 15:16:07 +0300

    [CloudMe#6] Code refactoring

---
+++ app/src/main/java/com/example/cloudme/activity/homeActivity/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.cloudme.activity.homeActivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.cloudme.cloudme.R;
+
+/**
+ * Created by Slavik on 21.04.2018.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+}
--- app/src/main/java/com/example/cloudme/cloudme/HomeActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.cloudme.cloudme;
-
-/**
- * Created by Slavik on 21.04.2018.
- */
-
-public class HomeActivity {
-}
