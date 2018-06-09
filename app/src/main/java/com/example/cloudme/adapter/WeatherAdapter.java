32f68c2ae716d5f5db209c19a959d0877ae60fdd
PavloKhmarnyi <pavlo.khmarny@gmail.com>
2018-06-09 23:35:13 +0300

    [CloudMe#36] Prepare adapter

--- app/src/main/java/com/example/cloudme/adapter/WeatherAdapter.java
+++ app/src/main/java/com/example/cloudme/adapter/WeatherAdapter.java
@@ -5,8 +5,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.cloudme.cloudme.R;
+import com.example.cloudme.service.openWeather.model.WeatherItem;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by localhost on 5/5/2018.
@@ -16,8 +21,9 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
     private final OnWeatherItemClickListener listener;
     private LayoutInflater inflater;
     private Context context;
+    private List<WeatherItem> weatherItems;
 
-    public WeatherAdapter(Context context, ArrayList<String> weatherList, OnWeatherItemClickListener listener) {
+    public WeatherAdapter(Context context, ArrayList<WeatherItem> weatherItems, OnWeatherItemClickListener listener) {
         this.listener = listener;
         this.context = context;
         this.inflater = LayoutInflater.from(context);
@@ -25,7 +31,9 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.weather_item, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
@@ -35,12 +43,15 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
 
     @Override
     public int getItemCount() {
-        return 0;
+        return weatherItems.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final View view;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            this.view = itemView;
         }
     }
 }
