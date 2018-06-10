707efac7036633db3cf827bc0c6ce19b6c086256
PavloKhmarnyi <pavlo.khmarny@gmail.com>
2018-06-10 13:10:16 +0300

    Merged CloudMe#36 into master

--- app/src/main/java/com/example/cloudme/adapter/WeatherAdapter.java
+++ app/src/main/java/com/example/cloudme/adapter/WeatherAdapter.java
@@ -40,7 +40,7 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
-        holder.dayWeatherItemTextView.setText(position + ".");
+        holder.dayWeatherItemTextView.setText(position + 1 + ".");
         holder.temperatureWeatherItemTextView.setText(weatherItems.get(position).getMain().getTemp() + " \u2109");
         holder.humidityWeatherItemTextView.setText(weatherItems.get(position).getMain().getHumidity() + " %");
         holder.dateWeatherItemTextView.setText(weatherItems.get(position).getDateTimeTxt());
